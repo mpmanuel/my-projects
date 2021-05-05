aac3ccdf7244437667d8b7b837da0cf073437db5
Mark <markpm721@gmail.com>
2021-05-05 18:53:57 -0500

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import MyProjects from "./components/MyProjects/MyProjects"
+import Pomodoro from "./components/Pomodoro/Pomodoro"
+import {BrowserRouter as Router, Link, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+        <li>
+           <Link to="/">Home</Link>
+         </li>
+         <li>
+           <Link to="/Pomodoro">Pomodoro</Link>
+         </li>
+        </ul>
+
+        <Switch>
+          
+          
+          <Route path="/pomodoro">
+            <Pomodoro/>
+          </Route>
+
+          <Route path="/" exact={true}>
+            <MyProjects/>
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MyProjects/MyProjects.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyProjects() {
+    return(<div>Home</div>);
+}
