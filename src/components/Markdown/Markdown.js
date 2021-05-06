d124084cbb7de6393fa818b9e98816c8920ab2bf
Mark <markpm721@gmail.com>
2021-05-05 19:43:28 -0500

    update styles for markdown

--- src/components/Markdown/Markdown.js
+++ src/components/Markdown/Markdown.js
@@ -1,19 +1,21 @@
-import React, { useState} from 'react';
-import './Markdown.css';
-import ReactMarkdown from 'react-markdown';
+import React, { useState } from "react"
+import "./Markdown.css"
+import ReactMarkdown from "react-markdown"
 
 export default function App() {
-  const [markdown, setMarkdown] = useState('# hello'); 
+  const [markdown, setMarkdown] = useState("# hello")
 
   function handleChange(e) {
-    setMarkdown(e.target.value);
+    setMarkdown(e.target.value)
   }
 
   return (
-    <div className="app">
-        <textarea onChange={handleChange} value={markdown}/>
+    <div className="markdown">
+      <div className="app">
+        <textarea onChange={handleChange} value={markdown} />
         <ReactMarkdown className="preview" source={markdown}></ReactMarkdown>
         {/* <ReactMarkdown className="preview" source={markdown}/> */}
+      </div>
     </div>
-  );
+  )
 }
